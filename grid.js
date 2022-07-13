e0033276faee1e130ca7b89b7cdb9458bc3c5951
Chantelle Perreau <cperreau@myune.edu.au>
2022-07-13 20:23:46 +1000

    Recompiled grid file

--- grid.js
+++ grid.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const row_1 = __importDefault(require("./Row"));
+const Row_1 = __importDefault(require("./Row"));
 class Grid {
     constructor(rowNumber, squareNumberPerRow) {
         var _a;
         this.rows = Array.from({ length: rowNumber }).map((_, index) => {
-            return new row_1.default(index, squareNumberPerRow);
+            return new Row_1.default(index, squareNumberPerRow);
         });
         this.element = document.createElement('div');
         this.element.classList.add('grid');
